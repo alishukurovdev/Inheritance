3d06a32807916f1057dcc9877ea2b14558a7e4a1
ALIWKA-44_ <sukurovali17@gmail.com>
2024-04-01 01:52:15 +0400

    base and this

---
+++ Device.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ThisAndBase;
+
+namespace ThisAndBase
+{
+    public class Device
+    { 
+        public int Width {  get; set; }
+        public int Height { get; set; }
+        public int Weight {  get; set; }
+
+        
+        public void OpenDevice()
+        {
+
+        }
+        public void CloseDevice()
+        {
+
+        }
+    }
+}
+//Device classı var  ++
+//Width, Height, Weight field-ları olur.  ++
+//OpenDevice və CloseDevice voidləri var. ++
+//Əgər DeviceOpen ilk dəfə və ya DeviceClose methodundan sonra işlənirsə ekrana “Device opening…” yazısını versin. 
+//Əgər bir neçə dəfə ardıcıl olaraq çağırılıbsa “Device already opened” yazılsın. Qanuna uyğunluq DeviceClose voidi üçün də keçərlidir.
---
+++ Laptop.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThisAndBase
+{
+    public class Laptop:Device
+    {
+        public int PortCount {  get; set; }
+
+        public Laptop(int portCount)
+        {
+            PortCount = portCount;
+        }
+    }
+}
+//Laptop:    ++
+//PortCount field-ı var.   ++
